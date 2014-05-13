c9d8b57c965b5d24725de41c5168d3c69823c3a2
Egor Nepomnyaschih <enepomnyaschih@issart.com>
2014-05-13 17:00:02 +0700

    Commit modern version.

--- game.js
+++ game.js
@@ -1,72 +1,41 @@
-var score = 0;       // Текущий счет
-var interval = 400;  // Интервал появления мишени в миллисекундах
-var maxTargets = 10; // Максимальное количество мишеней на экране
+var score = 0;
+var interval = 400;
+var maxTargets = 10;
 
-// Объявляем главную точку входа в программу с помощью jQuery
 $(function() {
-  
-  // Запускаем таймер появления мишеней
-  var timer = setInterval(function() {
-    
-    // Проверяем, если неразбитых мишеней на экране уже слишком много...
-    if ($('.target[status=alive]').length >= maxTargets) {
-      
-      // То удаляем панель с мишенями...
-      $('.panel').remove();
-      
-      // Пишем Game over...
-      $('.score-box').text('Game over. SCORE: ' + score);
-      
-      // Меняем стиль элемента .score-box на декоративный
-      $('.score-box').addClass('finished');
-      
-      // Останавливаем таймер
-      clearInterval(timer);
-      return;
-    }
-    
-    // Иначе создаем новую мишень
-    var target = $('<div class="target" status="alive"></div>');
-    
-    // Добавляем ее в панель
-    $('.panel').append(target);
-    
-    // Вычисляем размеры панели и мишени
-    var panelWidth = $('.panel').width();
-    var panelHeight = $('.panel').height();
-    var targetWidth = target.width();
-    var targetHeight = target.height();
-    
-    // Задаем случайные координаты для мишени
-    target.css({
-      left: (panelWidth - targetWidth) * Math.random() + 'px',
-      top: (panelHeight - targetHeight) * Math.random() + 'px'
-    });
-  }, interval);
-  
-  // Прослушиваем клик мышью по всем неразбитым мишеням внутри панели
-  $('.panel').on('mousedown', '.target[status=alive]', function(event) {
-    
-    // Получаем мишень, по которой кликнули
-    var target = $(event.currentTarget);
-    
-    // Меняем статус мишени на broken1
-    target.attr('status', 'broken1');
-    
-    // Запускаем таймер на 200 миллисекунд, чтобы поменять статус на broken2
-    setTimeout(function() {
-      target.attr('status', 'broken2');
-    }, 200);
-    
-    // Тут же запускаем таймер на 400 миллисекунд, чтобы удалить мишень
-    setTimeout(function() {
-      target.remove();
-    }, 400);
-    
-    // Увеличиваем счет
-    ++score;
-    
-    // Выводим счет на экран
-    $('.score').text(score);
-  });
+	var timer = setInterval(function() {
+		if ($('.target[status=alive]').length >= maxTargets) {
+			$('.panel').remove();
+			$('.score-box').text('Game over. SCORE: ' + score);
+			$('.score-box').addClass('finished');
+			clearInterval(timer);
+			return;
+		}
+		
+		var target = $('<div class="target" status="alive"><span class="target-1"></span><span class="target-2"></span><span class="target-3"></span><span class="target-4"></span></div>');
+		$('.panel').append(target);
+		
+		var panelWidth = $('.panel').width();
+		var panelHeight = $('.panel').height();
+		var targetWidth = target.width();
+		var targetHeight = target.height();
+		target.css({
+			left: (panelWidth - targetWidth) * Math.random() + 'px',
+			top: (panelHeight - targetHeight) * Math.random() + 'px'
+		});
+	}, interval);
+	
+	$('.panel').on('mousedown', '.target', function(event) {
+		var target = $(event.currentTarget);
+		target.attr('status', 'broken1');
+		setTimeout(function() {
+			target.attr('status', 'broken2');
+		}, 800);
+		setTimeout(function() {
+			target.remove();
+		}, 400);
+		
+		++score;
+		$('.score').text(score);
+	});
 });
